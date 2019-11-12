e33b03357a5c9ff72911262deeef502a9aa9c094
Mihail Mikov <mihail.mikov@skyscanner.net>
2019-11-12 11:54:58 +0200

    skyscanner board games meetup

--- src/presentation.js
+++ src/presentation.js
@@ -7,6 +7,10 @@ import {
   Cite,
   Deck,
   Heading,
+  Image,
+  Layout,
+  Fit,
+  Fill,
   ListItem,
   List,
   Quote,
@@ -19,13 +23,14 @@ import createTheme from 'spectacle/lib/themes/default';
 
 // Require CSS
 require('normalize.css');
+require('./overrides.css');
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
+    primary: 'black',
+    secondary: 'white',
+    tertiary: 'limegreen',
+    quaternary: 'goldenrod',
   },
   {
     primary: 'Montserrat',
@@ -37,57 +42,208 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
+        transition={['slide']}
+        transitionDuration={200}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
+        <Slide bgImage="images/collection-new.jpg">
+          <div class="blurred-background">
+            <Heading size={1} fit caps textColor="white">
+              Board games
+            </Heading>
+            <Heading size={2} fit caps textColor="white">
+              my new addiction
+            </Heading>
+          </div>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
+        <Slide>
+          <Heading size={4}>
+            Anatomy of a board game
           </Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Theme</ListItem>
+            <ListItem>Mechanisms</ListItem>
+            <ListItem>Components</ListItem>
+            <ListItem>Target audience</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <Slide bgColor="white">
+          <Heading size={4}>
+            Не се сърди човече (sorry!)
+          </Heading>
+          <Image src="images/sorry.jpeg" width={500} />
+        </Slide>
+        <Slide>
+          <Heading size={4} width={500}>
+            Monopoly
+          </Heading>
+          <Image src="images/monopoly.jpg" />
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            Classical board games
+          </Heading>
+          <List>
+            <ListItem>Chess - perfect information, abstract strategy</ListItem>
+            <ListItem>Backgammon - roll and move, abstract strategy</ListItem>
+            <ListItem>Scrabble - hand management, word game</ListItem>
+            <ListItem>Activity - acting, party game</ListItem>
+            <ListItem>Mastermind - deduction, paper and pencil</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            My top favorite board game mechansims
+          </Heading>
+          <List>
+            <ListItem>Variable player powers</ListItem>
+            <ListItem>Engine building</ListItem>
+            <ListItem>Card drafting</ListItem>
+            <ListItem>Tile placement</ListItem>
+            <ListItem>Worker placement</ListItem>
+            <ListItem>Set collection</ListItem>
+            <ListItem>Network building</ListItem>
+            <ListItem>Partnerships</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            Even more nice board game mechansims
+          </Heading>
+          <List>
+            <ListItem>Bluffing</ListItem>
+            <ListItem>Take that</ListItem>
+            <ListItem>Trick taking</ListItem>
+            <ListItem>Area control</ListItem>
+            <ListItem>Push your luck</ListItem>
+            <ListItem>Auctioning / Bidding</ListItem>
+            <ListItem>Grid / Point-to-Point Movement</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            My least favorite game mechansims
+          </Heading>
+          <List>
+            <ListItem>Player elimination</ListItem>
+            <ListItem>Roll nd move</ListItem>
+            <ListItem>Single dice rolls</ListItem>
+            <ListItem>Role playing / Storytelling</ListItem>
+            <ListItem>Memory / Trivia knowledge</ListItem>
+            <ListItem>Real-time</ListItem>
+          </List>
+        </Slide>
+        <Slide bgColor="white">
+          <Heading size={4}>
+            Не се сърди човече (sorry!)
+          </Heading>
+          <Layout>
+            <Fill>
+              <Image src="images/sorry.jpeg" width={500} />
+            </Fill>
+            <Fill>
+              <List textColor="black">
+                <ListItem>Mechanisms: roll and move, take that</ListItem>
+                <ListItem>Theme: abstract</ListItem>
+                <ListItem>Components: dies and meeples</ListItem>
+                <ListItem>Audience: children</ListItem>
+              </List>
+            </Fill>
+          </Layout>
+        </Slide>
+        <Slide>
+          <Heading size={4} width={500}>
+            Monopoly
+          </Heading>
+          <Layout>
+          <Fill>
+            <Image src="images/monopoly.jpg" />
+          </Fill>
+          <Fill>
+            <List>
+              <ListItem>Mechanisms: roll and move, set collection, player elimination, auctioning</ListItem>
+              <ListItem>Theme: economic</ListItem>
+              <ListItem>Components: board, cards, dies and meeples</ListItem>
+              <ListItem>Audience: any</ListItem>
+            </List>
+          </Fill>
+          </Layout>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            Some of the most popular gateway games
+          </Heading>
+          <List>
+            <ListItem>Dixit</ListItem>
+            <ListItem>Catan</ListItem>
+            <ListItem>Ticket to ride</ListItem>
+            <ListItem>Carcassonne</ListItem>
+            <ListItem>Citadels</ListItem>
+            <ListItem>Camel up</ListItem>
+            <ListItem>Saboteur</ListItem>
+            <ListItem>Munchkin</ListItem>
+            <ListItem>Azul</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            Some of my favorite games
+          </Heading>
+          <List>
+            <ListItem>Race for the Galaxy</ListItem>
+            <ListItem>Scythe</ListItem>
+            <ListItem>Keyflower</ListItem>
+            <ListItem>The castles of Burgundy</ListItem>
+            <ListItem>Istanbul</ListItem>
+            <ListItem>7 wonders</ListItem>
+            <ListItem>Architects of the West Kingdom</ListItem>
+            <ListItem>Orléans</ListItem>
+            <ListItem>Mascarade</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            Simultanious player actions
+          </Heading>
+          <List>
+            <ListItem>BigO(1)</ListItem>
+            <ListItem>Mechanisms: card drafting, action selection</ListItem>
+            <ListItem>Games: 7 wonders, Race for the galaxy</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            Deception
+          </Heading>
+          <List>
+            <ListItem>Social and fun</ListItem>
+            <ListItem>Mechanisms: secret roles, bluffing, partnerships</ListItem>
+            <ListItem>Games: Secret Hitler, Mascarade</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={4}>
+            Cooperative
+          </Heading>
+          <List>
+            <ListItem>Players vs game</ListItem>
+            <ListItem>Problem solving</ListItem>
+            <ListItem>Games: Pandemic, Hanabi</ListItem>
+          </List>
+        </Slide>
+        <Slide bgColor="white">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>Sometimes win, sometime lun</Quote>
+            <Cite>Hristo Stoichkov</Cite>
           </BlockQuote>
+        </Slide>
+        <Slide>
+          <Heading size={1}>
+            Thank you!
+          </Heading>
+          <Heading size={3} textColor="white !important">
+            Let's play some games!
+          </Heading>
         </Slide>
       </Deck>
     );
